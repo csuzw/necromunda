ce7e693f3a698aa9a8f750e7b0032832dc81970d
csuzw <rjcarrington@gmail.com>
2022-06-19 12:12:26 +0100

    Added gang image

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,6 @@ import React from 'react';
 
 export default function Home() {
     return (
-        <h1>{process.env.REACT_APP_GANG_NAME}</h1>
+        <img className="main-image" src={process.env.PUBLIC_URL + '/img/gang.jpg'} alt="The gang" />
     );
 }
