2a8ed534049a55de9fcbb2e7fd00f24f2628ff2a
csuzw <rjcarrington@gmail.com>
2022-07-10 23:48:30 +0100

    Added tooltips to images

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 export default function Gallery(props) {
     return (
         <div className="gallery">
-            <img className="main-image" src={process.env.PUBLIC_URL + '/img/gang.jpg'} alt="The gang" />
+            <img className="main-image" src={process.env.PUBLIC_URL + '/img/gang.jpg'} alt={process.env.REACT_APP_GANG_NAME} title={process.env.REACT_APP_GANG_NAME} />
             <div className="fighter-images">
-                {props.gang.fighters.filter(fighter => fighter.image).map((fighter, i) => <img className="fighter-image" key={i} src={process.env.PUBLIC_URL + fighter.image} alt={fighter.name} />)}
+                {props.gang.fighters.filter(fighter => fighter.image).map((fighter, i) => <img className="fighter-image" key={i} src={process.env.PUBLIC_URL + fighter.image} alt={fighter.name} title={fighter.name} />)}
             </div>
         </div>
     );
