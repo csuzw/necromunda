efcbe562b5e1dd80a49d444bc9a0f74e53e8f172
csuzw <rjcarrington@gmail.com>
2022-09-12 18:19:11 +0100

    Change styling for improved printing

--- src/components/GangNarrative.jsx
+++ src/components/GangNarrative.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function GangNarrative(props) {
     return (
-        <div class="gang-narrative">
+        <div className="gang-narrative">
             <h3>Servants of the Shrike</h3>
             <p>Her actions were considered too extreme even for a House of fanatics and zealots.  Her followers believe she sees the future, the true path of the Redemption.</p>
             <p>She is the butcher bird, the Shrike, Mother Excubita.  While the powers that be in the House of Faith have cast her out, there are still those among the brothers and sisters of the Redemption who believe she has been divinely blessed by the God-Emperor.</p>
--- src/components/GangRosterSummary.jsx
+++ src/components/GangRosterSummary.jsx
@@ -3,33 +3,29 @@ import React from 'react';
 import { gangHelper } from '../data/helpers/gangHelper';
 
 export default function GangRosterSummary(props) {
+
+    let stashWeaponsDisplayText = gangHelper.getStashWeaponsDisplayText(props.gang);
+    let stashWargearDisplayText = gangHelper.getStashWargearDisplayText(props.gang);
+    let territories = props.gang.territories.length ? props.gang.territories : [ "\u200b", "\u200b", "\u200b", "\u200b", "\u200b" ];
     return (
         <div className="gang-summary">
             <div className="gang-name gang-summary-group">
                 <div className="text-label">Gang Name</div>
                 <div className="text-value">{props.gang.name}</div>
-            </div>
-            <div className="gang-house gang-summary-group">
                 <div className="text-label">House</div>
                 <div className="text-value">{gangHelper.getHouseDisplayText(props.gang)}</div>
             </div>
             <div className="gang-values gang-summary-group">
-                <div className="gang-rating">
-                    <div className="text-label text-sm">Gang Rating</div>
+                    <div className="text-label">Gang Rating</div>
                     <div className="text-value text-numeric">{gangHelper.getRatingDisplayText(props.gang)}</div>
-                </div>
-                <div className="gang-reputation">
-                    <div className="text-label text-sm">Reputation</div>
+                    <div className="text-label">Reputation</div>
                     <div className="text-value text-numeric">{props.gang.reputation}</div>
-                </div>
-                <div className="gang-wealth">
-                    <div className="text-label text-sm">Wealth</div>
+                    <div className="text-label">Wealth</div>
                     <div className="text-value text-numeric">{gangHelper.getWealthDisplayText(props.gang)}</div>
-                </div>
             </div>
             <div className="gang-territories gang-summary-group">
                 <div className="text-label">Territories Held</div>
-                {props.gang.territories.map((territory, i) => <div className="text-value" key={i}>{territory}</div>)}               
+                {territories.map((territory, i) => <div className="text-value" key={i}>{territory}</div>)}               
             </div>
             <div className="gang-stash gang-summary-group">
                 <div className="text-label">Stash</div>
@@ -39,11 +35,11 @@ export default function GangRosterSummary(props) {
                 </div>
                 <div className="gang-stash-weapons">
                     <div className="text-label text-sm text-left">Weapons</div>
-                    <div className="text-value">{gangHelper.getStashWeaponsDisplayText(props.gang)}</div>
+                    <div className="text-value">{(stashWeaponsDisplayText) ? stashWeaponsDisplayText : "\u200b"}</div>
                 </div>
                 <div className="gang-stash-wargear">
                     <div className="text-label text-sm text-left">Wargear</div>
-                    <div className="text-value">{gangHelper.getStashWargearDisplayText(props.gang)}</div>
+                    <div className="text-value">{(stashWargearDisplayText) ? stashWargearDisplayText : "\u200b"}</div>
                 </div>
             </div>
         </div>
