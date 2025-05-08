ecfe4915747d4a5c54380994aa2b01388ac9639c
csuzw <rjcarrington@gmail.com>
2025-05-08 17:55:48 +0100

    Change gallery

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -6,13 +6,13 @@ export default function Gallery(props) {
         <div className="gallery">
 
             <GangNarrative />
-            
-            <img className="main-image" src={process.env.PUBLIC_URL + '/img/gang.jpg'} alt={process.env.REACT_APP_GANG_NAME} title={process.env.REACT_APP_GANG_NAME} />
-
 
             <div className="fighter-images">
                 {props.gang.fighters.filter(fighter => fighter.image).map((fighter, i) => <img className="fighter-image" key={i} src={process.env.PUBLIC_URL + fighter.image} alt={fighter.name} title={fighter.name} />)}
             </div>
+            
+            <img className="main-image" src={process.env.PUBLIC_URL + '/img/gang.jpg'} alt={process.env.REACT_APP_GANG_NAME} title={process.env.REACT_APP_GANG_NAME} />
+
         </div>
     );
 }
